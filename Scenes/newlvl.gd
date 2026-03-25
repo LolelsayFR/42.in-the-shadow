e825b95fc9ed697b65a38ea978a603f78ccaae30
emaillet <maillet.emilien@gmail.com>
2026-03-25 17:51:03 +0100

    Add: Unlocked animation 🦔

--- Scenes/newlvl.gd
+++ Scenes/newlvl.gd
@@ -6,13 +6,19 @@ func _ready() -> void:
 	pass # Replace with function body.
 
 
+var factor:int = 1
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	if G.notifyNewLvl && G.gameState == G.MAIN:
-		$PanelContainer/Blur/Left/TitlePage.text = "New level : %d" % (G.ProgressLvl + 1)
+		$PanelContainer/Blur/Left/TitlePage.text = "Level %d unlocked" % (G.ProgressLvl + 1)
 		visible = true
 		G.lvl = G.ProgressLvl
 		G.notifyNewLvl = false
+	$PanelContainer/Blur/Left/TitlePage.position.y = clamp((delta * factor) * 40 + $PanelContainer/Blur/Left/TitlePage.position.y, -10, 10)
+	if $PanelContainer/Blur/Left/TitlePage.position.y >= 10:
+		factor = -1
+	if $PanelContainer/Blur/Left/TitlePage.position.y <= -10:
+		factor = 1
 	pass
 
 
